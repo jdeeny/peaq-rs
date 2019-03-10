9bd1d4a94cf43f3a7ea0154f7ee6f9ae50b3cb66
jdeeny <jdeeny@gmail.com>
2019-03-09 21:04:48 -0700

    Add header to leveladapter

---
+++ src/leveladapter.rs
@@ -0,0 +1,5 @@
+/* #PeaqLevelAdapter encapsulates the level and pattern adaptation described in
+* section 3.1 of <xref linkend="BS1387" />. It estimates the per-band level
+* differences between reference and test signal and adapts them to each other
+* to compensate level differences and linear distortions.
+*/
